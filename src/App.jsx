79a586bd22ff14ed20c00b9e731fd5c99fce62e1
mahamahe2006 <mahamahe2006@gmail.com>
2026-06-17 12:44:29 +0530

    Add Jobs

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,28 @@ import ApplyJob from './pages/ApplyJob'
 import Applications from './pages/Applications'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
+import DashBoard from './pages/DashBoard'
+import AddJobs from './pages/AddJobs'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
+import 'quill/dist/quill.snow.css'
 
 const App = () => {
   const { showRecruiterLogin } = useContext(AppContext);
   return (
     <div>
-      {showRecruiterLogin && <RecruiterLogin/>}
+      {showRecruiterLogin && <RecruiterLogin />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/apply-job/:id' element={<ApplyJob />} />
         <Route path='/applications' element={<Applications />} />
+        <Route path='/dashboard' element={<DashBoard />} >
+          <Route path='add-job' element={<AddJobs />} />
+          <Route path='manage-job' element={<ManageJobs />} />
+          <Route path='view-applications' element={<ViewApplications />} />
+        </Route>
       </Routes>
-    </div>
+    </div >
   )
 }
 
