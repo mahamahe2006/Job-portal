ccb49aa7385801d4a63b4598402b1c1d963d403c
mahamahe2006 <mahamahe2006@gmail.com>
2026-07-11 22:52:18 +0530

    refactor(server): register user routes and middleware

--- server/app.js
+++ server/app.js
@@ -5,15 +5,24 @@ import 'dotenv/config'
 import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node"
 import { clerkWbhooks } from './controllers/webhooks.js'
+import companyRoutes from './routes/companyRoutes.js'
+import connectCloudinary from './config/cloudinary.js'
+import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
+
 
 // Initialize express
 const app = express()
 
 // Connect to DB
 await connectDB()
+await connectCloudinary()
 
 // Middlewares
 app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
 
 // Webhook route MUST come before express.json()
 app.post(
@@ -22,8 +31,12 @@ app.post(
   clerkWbhooks
 )
 
-// Then use express.json()
-app.use(express.json())
+
+app.use('/api/company',companyRoutes)
+app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
+app.use(clerkMiddleware ())
+
 
 // Routes
 app.get('/', (req, res) => res.send("API Working"))
