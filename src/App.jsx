345f1137f2869525ec2d29d49be406763f19d283
mahamahe2006 <mahamahe2006@gmail.com>
2026-07-12 10:44:38 +0530

    Fix Home import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import ApplyJob from './pages/ApplyJob'
 import Applications from './pages/Applications'
 import RecruiterLogin from './components/RecruiterLogin'
